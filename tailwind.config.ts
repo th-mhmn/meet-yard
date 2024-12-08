09cee7f078f9e37475600ae0009d36ba584387eb
taha <tahamehmandoust@gmail.com>
2024-12-09 00:31:46 +0330

    add colors and background image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        hero: "url('/images/hero-background.webp')",
       },
       borderRadius: {
         lg: 'var(--radius)',
@@ -27,7 +32,16 @@ const config: Config = {
           4: '#1E2757',
         },
         blue: {
-          1: '#0E78F9',
+          1: '#537FE7',
+        },
+        sky: {
+          1: '#c9ddff',
+        },
+        cards: {
+          1: '#FFE162',
+          2: '#FF9668',
+          3: '#DB456F',
+          4: '#A74FAF',
         },
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
