6dcbfd49d8dbb221351ee9d4e2c79182e902523d
taha <tahamehmandoust@gmail.com>
2024-12-09 19:48:09 +0330

    implement video call stream

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button'
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void
+}) => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false)
+
+  const call = useCall()
+
+  if (!call) {
+    throw new Error('useCall must be used within StreamCall component')
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable()
+      call?.microphone.disable()
+      return
+    }
+    call?.camera.enable()
+    call?.microphone.enable()
+  }, [isMicCamToggledOn, call?.camera, call?.microphone])
+
+  return (
+    <div className="flex-center flex-col h-screen w-full text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex-center h-16 gap-3">
+        <label className="flex-center gap-2 font-medium text-white">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        onClick={() => {
+          call.join()
+          setIsSetupComplete(true)
+        }}
+        className="rounded-md bg-green-500 px-4 py-2.5"
+      >
+        Join Meeting
+      </Button>
+    </div>
+  )
+}
+
+export default MeetingSetup
