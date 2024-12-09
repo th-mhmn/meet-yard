34819e9b2c211761f2cd4418841cf7d2911d355d
taha <tahamehmandoust@gmail.com>
2024-12-09 19:48:28 +0330

    fix hero background

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   const date = new Intl.DateTimeFormat([], { dateStyle: 'full' }).format(now)
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover ">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover bg-center">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
             Upcoming Meeting at{' '}
